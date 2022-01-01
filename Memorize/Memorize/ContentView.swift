50134a52b1eb437a3b81fcfb0663b7792f8d7e49
Faking R3Dz <tahanoor78655@gmail.com>
2022-01-01 20:15:09 +0500

    This is the Initial setup for the project

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Taha Noor on 1/1/22.
+//  Copyright © 2022 Taha Noor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
